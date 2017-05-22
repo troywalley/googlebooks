0907e28868948a6661434d77f61ab4462a8c1131
Troy Walley <walley.troy@yahoo.com>
2017-05-22 13:45:21 -0400

    recent changes

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -11,12 +11,15 @@ function GetAjax(book){
     dataType: "json",
     success: function(response) {
         response.items.forEach(function(response){
-          var div=document.createElement("a")
+          var div=document.createElement("div")
           var container=document.getElementsByClassName("results-container")[0]
           var form=document.createElement("form")
-
+          var input=document.createElement("input")
           form.action="/search"
           form.method="post"
+          input.type="hidden"
+          input.value=response
+          form.appendChild(input)
           div.innerHTML=response.volumeInfo.title
           container.appendChild(div)
 
