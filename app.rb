542b8070df326d92f21ceb3c3e5fa44556fb1790
Sam Almeida <samRAlmeida@icloud.com>
2017-05-22 16:33:12 -0400

    signin/out routes

--- app.rb
+++ app.rb
@@ -1,12 +1,23 @@
-# get '/' do
-#   erb :sign_in
-# end
-
+before  do
+	@current_user = session[:user_id] if session[:user_id]
+end
+	
+end
 get '/' do
-  erb :home
+
+  erb :sign_in
 end
 
+get '/sign_up' do
+	
+	erb :sign_up
+end
 
+post '/sign_in' do	
+end
+
+post '/sign_up' do
+end
 
 post '/home' do
   url=params[:url]
