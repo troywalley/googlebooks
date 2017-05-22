5c45d1a372b178a782579ce7e62136e4d0fbd13f
Sam Almeida <samRAlmeida@icloud.com>
2017-05-22 13:52:57 -0400

    attempt to push

--- app.rb
+++ app.rb
@@ -6,6 +6,13 @@ require 'sinatra/flash'
 set :database, "sqlite3:googlebooks.sqlite3"
 set :sessions, true
 require './models'
+
+
+get '/' do
+
+	erb :home
+end
+
 # get '/' do
 #   erb :sign_in
 # end
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,39 @@
-alert("working")
+$("document").ready(function() {
+
+
+
+
+
+
+
+ $.ajax({
+	 method: "GET",
+	 url: "https://www.googleapis.com/books/v1/volumes",
+
+	 data: {
+	    q: "harry potter"
+	},
+
+	datatype: "json",
+	success: function(response){   
+	     console.log(response)
+	     
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
