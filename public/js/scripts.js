90e0b02b804a5ffb3fb5b067148d025380a211fa
Joshua Neidich <Joshua@reyoumassage.com>
2017-05-22 13:44:02 -0400

    ajax script

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,31 +1,12 @@
-function GetAjax(book){
- $.ajax({
-      method: "get",
-    url: "https://www.googleapis.com/books/v1/volumes",
-
-
-    data: {
-        q: book
-    },
-
-    dataType: "json",
-    success: function(response) {
-        response.items.forEach(function(response){
-          var div=document.createElement("a")
-          var container=document.getElementsByClassName("results-container")[0]
-          var form=document.createElement("form")
-
-          form.action="/search"
-          form.method="post"
-          div.innerHTML=response.volumeInfo.title
-          container.appendChild(div)
-
-        })
-
+xhttp.open("GET", "https://www.googleapis.com/books/v1/volumes", true);
+xhttp.send();
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
     }
-
-})
-
+  };
+  xhttp.open("GET", "demo_get.asp", true);
+  xhttp.send();
 }
-
-GetAjax("Harry Potter")
